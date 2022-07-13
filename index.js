39d02c1ea4dd01536adaf84e37377c2dbd81a7dc
Guilherme Pacheco <guilherme.pacheco@pontta.com>
2022-07-13 15:48:01 -0300

    Add novos projetos Dinabox

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var express = require("express");
 const app = express();
+const PORT = 8877;
+
+const DEFAULT_PROJECT_ID = "5011841680";
+const PROJECTS = [DEFAULT_PROJECT_ID, "0206700929", "0841601175"];
 
 app.set("json spaces", 2);
 
@@ -8,9 +12,21 @@ app.get("/projects", function (req, res) {
   res.sendFile(__dirname + "/files/projects-search-response.json");
 });
 
-app.get("/project", function (req, res) {
+function resolveProjectId(id) {
+  return PROJECTS.includes(id) ? id : DEFAULT_PROJECT_ID;
+}
+
+function sendProject(id, res) {
   res.header("Content-Type", "application/json");
-  res.sendFile(__dirname + "/files/project-response.json");
+  res.sendFile(`${__dirname}/files/project-${resolveProjectId(id)}-response.json`);
+}
+
+app.get("/project/:id", function (req, res) {
+  sendProject(req.params.id, res);
+});
+
+app.get("/project", function (req, res) {
+  sendProject(DEFAULT_PROJECT_ID, res);
 });
 
 app.get("/token", function (req, res) {
@@ -23,4 +39,4 @@ app.post("/token", function (req, res) {
   res.sendFile(__dirname + "/files/token-success.json");
 });
 
-app.listen(8877, () => console.log(`Serving localhost:${8877}`));
+app.listen(PORT, () => console.log(`Serving localhost:${PORT}`));
